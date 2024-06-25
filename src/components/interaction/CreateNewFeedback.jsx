026d7602d135cb0352d84120225db077bd2f202b
yebology <yobelnathaniel12@gmail.com>
2024-06-25 15:54:46 +0700

    working on create new feedback

--- src/components/interaction/CreateNewFeedback.jsx
+++ src/components/interaction/CreateNewFeedback.jsx
@@ -1,9 +1,120 @@
 import React from 'react'
 
 const CreateNewFeedback = () => {
-  return (
-    <div>CreateNewFeedback</div>
-  )
+
+    const handleSubmit = () => {
+
+    }
+
+    const onClose = () => {
+        setGlobalState("createScale", "scale-0");
+        reset();
+      };
+    
+      const reset = () => {
+        // setTitle("");
+        // setDescription("");
+        // setImageURL("");
+        // setAmount("");
+        // setDate("");
+      };
+
+    return (
+        <div
+          className={`fixed flex items-center justify-center w-screen h-screen inset-0 bg-black bg-opacity-50 transform transition-transform duration-300 ${createScale}`}
+        >
+          <div className="bg-white shadow-xl shadow-black rounded-xl w-11/12 md:w-2/5 h-7/12 p-6">
+            <form className="flex flex-col" onSubmit={handleSubmit} method="POST">
+              <div className="flex justify-between items-center">
+                <p className="font-semibold">Create Project</p>
+                <button
+                  type="button"
+                  className="border-0 bg-transparent focus:outline-none"
+                  onClick={onClose}
+                >
+                  <FaTimes />
+                </button>
+              </div>
+              <div className="flex justify-center items-center mt-5">
+                <div className="rounded-xl overflow-hidden w-20 h-20">
+                  <img
+                    src={
+                      imageURL ||
+                      "https://www.hdwallpapers.in/download/cell_biology_background_hd_wallpaper_cellular-HD.jpg"
+                    }
+                    alt="title"
+                    className="h-full w-full object-cover cursor-pointer"
+                  />
+                </div>
+              </div>
+              <div className="flex justify-between items-center mt-5 rounded-xl bg-gray-300">
+                <input
+                  className="block w-full bg-transparent border-0 text-sm py-3 px-3 text-slate-500 focus:outline focus:ring-0"
+                  type="text"
+                  name="title"
+                  placeholder="Project Title"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center mt-5 rounded-xl bg-gray-300">
+                <input
+                  className="block w-full bg-transparent border-0 text-sm py-3 px-3 text-slate-500 focus:outline focus:ring-0"
+                  type="number"
+                  step={0.01}
+                  min={0.01}
+                  name="amount"
+                  placeholder="Amount (ETH)"
+                  onChange={(e) => setAmount(e.target.value)}
+                  value={amount}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center mt-5 rounded-xl bg-gray-300">
+                <input
+                  className="block w-full bg-transparent border-0 text-sm py-3 px-3 text-slate-500 focus:outline focus:ring-0"
+                  type="date"
+                  name="date"
+                  placeholder="Expires at"
+                  onChange={(e) => setDate(e.target.value)}
+                  value={date}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center mt-5 rounded-xl bg-gray-300">
+                <input
+                  className="block w-full bg-transparent border-0 text-sm py-3 px-3 text-slate-500 focus:outline focus:ring-0"
+                  type="url"
+                  name="imageURL"
+                  placeholder="Image URL"
+                  onChange={(e) => setImageURL(e.target.value)}
+                  value={imageURL}
+                  required
+                />
+              </div>
+              <div className="flex justify-between items-center mt-5 rounded-xl bg-gray-300">
+                <textarea
+                  className="block w-full bg-transparent border-0 text-sm py-3 px-3 text-slate-500 focus:outline focus:ring-0"
+                  type="text"
+                  name="description"
+                  placeholder="Project Description"
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="inline-block px-6 py-2.5 mt-5 text-white font-medium rounded-full shadow-md bg-indigo-600 hover:bg-indigo-700"
+              >
+                {" "}
+                Add Project{" "}
+              </button>
+            </form>
+          </div>
+        </div>
+      );
 }
 
 export default CreateNewFeedback
